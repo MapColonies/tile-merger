dbf9e73339a72539853864dbc8f1b5e5af193571
shahar s <cl_shaharshavit@rnd-hub.com>
2021-01-14 14:13:49 +0200

    feat: added code to merge png tiles

---
+++ src/tileMarger/models/tileMerger.ts
@@ -0,0 +1,37 @@
+import Jimp from 'jimp';
+const mergeTiles = async (target: string, ...files: string[]): Promise<void> => {
+  const tiles: Promise<Jimp>[] = [];
+  console.log(files);
+  for (let i = 0; i < files.length; i++) {
+    //validate that all images are png
+    if (!(files[i].endsWith('png') || files[i].endsWith('PNG'))) {
+      throw new Error('all tiles must be png');
+    }
+    const tile = Jimp.read(files[i]);
+    console.log(tile);
+    tiles.push(tile);
+  }
+  const loadedTiles = await Promise.all(tiles);
+  validate(loadedTiles);
+  //merge all images first is on the bottom
+  const composed = loadedTiles[0].clone();
+  for (let i = 1; i < tiles.length; i++) {
+    composed.composite(loadedTiles[i], 0, 0);
+  }
+  await composed.writeAsync(target);
+};
+
+const validate = (tiles: Jimp[]) => {
+  console.log(tiles.length);
+  const minTiles = 2;
+  if (tiles.length < minTiles) throw new Error('only 1 tile');
+  const width = tiles[0].getWidth();
+  const height = tiles[0].getHeight();
+  for (let i = 1; i < tiles.length; i++) {
+    if (tiles[i].getWidth() !== width || tiles[i].getHeight() != height) {
+      throw new Error('all tiles must have the same size');
+    }
+  }
+};
+
+export { mergeTiles };
